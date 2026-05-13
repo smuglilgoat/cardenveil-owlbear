aeb1c6e1eaee0f4a3838cdb8e6fd0e2e98be2b1e
AhmedMoussaouiSeg <ahmed.moussaoui@segula.fr>
2026-05-13 14:51:23 +0200

    This is the good one

--- src/hand.js
+++ src/hand.js
@@ -1,5 +1,5 @@
 import { mount } from 'svelte';
-import './app.css';
+import './hand.css';
 import HandPopover from './HandPopover.svelte';
 
 const app = mount(HandPopover, { target: document.getElementById('hand') });
