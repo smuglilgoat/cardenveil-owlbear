95e5ba773d2c746d60ac861a6dccfc4ff293d6dd
AhmedMoussaouiSeg <ahmed.moussaoui@segula.fr>
2026-09-08 13:59:25 +0200

    fix: sheet popover anchor

--- src/lib/CharacterSheet.svelte
+++ src/lib/CharacterSheet.svelte
@@ -439,7 +439,7 @@
         url: `${window.location.origin}/sheet.html?playerId=${encodeURIComponent(playerId)}&roomId=${encodeURIComponent(roomId)}&popoverId=${encodeURIComponent(SHEET_POPOVER_ID)}`,
         width: 380,
         height: 540,
-        anchorPosition: { left: vw, top: 0 },
+        anchorPosition: { left: vw, top: -70 },
         anchorOrigin: { horizontal: 'RIGHT', vertical: 'TOP' },
         transformOrigin: { horizontal: 'RIGHT', vertical: 'TOP' },
         disableClickAway: true,
